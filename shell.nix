0b4013900695c7d67e828410fed7c6a322ab4ddf
agranea <agranea@outlook.de>
2021-05-29 19:35:30 +0000

    cleanup, install exercism

--- shell.nix
+++ shell.nix
@@ -1,5 +1,5 @@
 { pkgs ? import <nixpkgs> {} }:
   pkgs.mkShell {
     # nativeBuildInputs is usually what you want -- tools you need to run
-    nativeBuildInputs = with pkgs; [ stack ghc ];
+    nativeBuildInputs = with pkgs; [ stack ghc cabal-install exercism ];
 }
